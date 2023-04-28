ab6b8c212a2c07aa6570e3bf91907439bc134f63
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-28 12:46:54 +0400

    Android paddings

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,44 +1,45 @@
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
-actual fun getPlatformName(): String = "Android"
 
 @Composable
 fun MainView() {
     TransparentSystemBars()
     Box(Modifier.fillMaxSize()) {
-        Column(Modifier.align(Alignment.TopStart)) {
-            repeat(5) {
-                Text(it.toString())
-            }
-        }
 
-        Column(Modifier.align(Alignment.BottomStart)) {
-            repeat(5) {
-                Text(it.toString())
-            }
+        Text("displayCutout", Modifier.displayCutoutPadding().align(Alignment.TopEnd))
+        Text("statusBars", Modifier.statusBarsPadding().align(Alignment.TopStart))
+
+        Text("navigationBars", Modifier.navigationBarsPadding().align(Alignment.BottomCenter))
+        Text("ime", Modifier.imePadding().background(Color.Yellow).align(Alignment.BottomStart))
+
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+            var textStr by remember { mutableStateOf("TextField to show keyboard") }
+            TextField(textStr, { textStr = it })
+
+            Text(
+                "safeGestures",
+                Modifier.fillMaxWidth().safeGesturesPadding().background(Color.LightGray)
+            )
         }
     }
-    if(false)Box(
-        Modifier
-            .background(Color.Yellow)
-            .displayCutoutPadding()//cutout
-            .background(Color.Blue)
-            .navigationBarsPadding()//bottom
-            .imePadding()//keyboard
-            .background(Color.LightGray)
-            .fillMaxSize()
-    ) {
 
-    }
 }
